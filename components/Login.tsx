7355940bd98077eb2936c505302cd644e5145d44
Ashwant Singh <ashwantconset@gmail.com>
2026-08-22 16:46:12 +0530

    added login page and api call with google login

---
+++ components/Login.tsx
@@ -0,0 +1,101 @@
+"use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button, Card, Divider, Form, Input } from "antd";
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "./shared/Logo";
+import { ArrowRightOutlined, GoogleOutlined } from "@ant-design/icons";
+import "@ant-design/v5-patch-for-react-19";
+import Link from "next/link";
+import { getSession, signIn } from "next-auth/react";
+
+import { useRouter } from "next/navigation";
+import clienCatchError from "@/libs/client-catch-error";
+
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const login = async (value: any) => {
+    try {
+      setLoading(true);
+      const payload = {
+        ...value,
+        redirect: false,
+      };
+      await signIn("credentials", payload);
+      const session = await getSession();
+
+      if (!session) throw new Error("failed to login user");
+
+      if (session.user.role === "user") return router.replace("/");
+
+      if (session.user.role === "admin") return router.replace("/admin/orders");
+    } catch (err) {
+      clienCatchError(err);
+    } finally {
+      setLoading(true);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    const payload = {
+      redirect: true,
+      callbackUrl: "/",
+    };
+
+    const res = await signIn("google", payload);
+  };
+  return (
+        <div className='bg-gray-100 h-screen grid grid-cols-2 animated__animated animate__fadeIn overflow-hidden'>
+            <div className='relative'>
+                <Image 
+                    src="/images/signup.jpg"
+                    fill
+                    alt="signup"
+                    className='object-cover'
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    priority
+                />
+            </div>
+            <div className='flex items-center justify-center'>
+                <Card className='w-[480px] animate__animated animate__slideInRight'>
+                    <div className='space-y-4'>
+                        <div className='flex justify-center'>
+                            <Logo />
+                        </div>
+                        <Form layout='vertical' onFinish={login}>
+                            <Form.Item 
+                                label="Email"
+                                name="email"
+                                rules={[{required: true, type: 'email'}]}
+                            >
+                                <Input size='large' placeholder='email@example.com' />
+                            </Form.Item>
+
+                            <Form.Item 
+                                label="Password"
+                                name="password"
+                                rules={[{required: true}]}
+                            >
+                                <Input.Password size='large' placeholder='email@example.com' />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button loading={loading} htmlType='submit' size='large' type="primary" icon={<ArrowRightOutlined />} className='!bg-violet-500 hover:!bg-violet-600'>Sign in</Button>
+                            </Form.Item>
+                        </Form>
+                        <Divider />
+                        <Button onClick={signInWithGoogle} icon={<GoogleOutlined />} size='large' className='!w-full' type="primary" danger>Signin with Google</Button>
+                        <div className='flex gap-2'>
+                            <p className='text-gray-500'>Don`t have an acount ?</p>
+                            <Link href="/signup">Sign up</Link>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        </div>
+    )
+}
+
+export default Login
