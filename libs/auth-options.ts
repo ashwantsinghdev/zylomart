2054aa7f0120f10341bf97b4d75aa5b6392331dc
Ashwant Singh <ashwantconset@gmail.com>
2026-08-22 11:02:02 +0530

    implemented session and auth handler or types interfaces of session and user

---
+++ libs/auth-options.ts
@@ -0,0 +1,91 @@
+import { NextAuthOptions, Session } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import axios from "axios";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", name: "email" },
+        password: { label: "Password", name: "password" },
+      },
+
+      async authorize(credentials) {
+        try {
+          const payload = {
+            email: credentials?.email,
+            password: credentials?.password,
+          };
+
+          const { data } = await axios.post(
+            `${process.env.SERVER}/api/user/login`,
+            payload,
+          );
+          return data;
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            return null;
+          }
+        }
+      },
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+    error: "/auth-failed",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "google") {
+        try {
+          const payload = {
+            email: user.email,
+            provider: "google",
+          };
+          const { data } = await axios.post(
+            `${process.env.SERVER}/api/user/login`,
+            payload,
+          );
+          user.id = data.id;
+          user.email = data.email;
+          user.name = data.name;
+          user.role = data.role;
+          user.address = data.address;
+          return true;
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            return false;
+          }
+        }
+      }
+      return true;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+        token.address = user.address;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+        session.user.address = token.address as Session["user"]["address"];
+      }
+      return session;
+    },
+  },
+  secret:process.env.NEXTAUTH_SECRET
+};
