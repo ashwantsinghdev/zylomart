e7981a2c327622ad6fa33ad0365da1cdb9c452e2
Ashwant Singh <ashwantconset@gmail.com>
2026-08-22 15:57:54 +0530

    added signup page ui and api call

---
+++ components/Signup.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { Button, Card, Form, Input } from "antd";
+import Image from "next/image";
+import Logo from "./shared/Logo";
+import { UserAddOutlined } from "@ant-design/icons";
+import "@ant-design/v5-patch-for-react-19";
+import Link from "next/link";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import clienCatchError from "@/libs/client-catch-error";
+
+const Signup = () => {
+  const router = useRouter();
+
+  const signup = async (value: any) => {
+    try {
+      await axios.post("/api/user/signup", value);
+      router.push("/login");
+    } catch (err) {
+      clienCatchError(err);
+    }
+  };
+
+  return (
+         <div className='bg-gray-100 h-screen grid grid-cols-2 animated__animated animate__fadeIn overflow-hidden'>
+             <div className='relative'>
+                 <Image
+                     src="/images/signup.jpg"
+                     fill
+                     alt="signup"
+                     className='object-cover'
+                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                     priority
+                 />
+             </div>
+             <div className='flex items-center justify-center'>
+                 <Card className='w-[480px] animate__animated animate__slideInRight'>
+                     <div className='space-y-4'>
+                         <div className='flex justify-center'>
+                             <Logo />
+                         </div>
+                         <Form layout='vertical' onFinish={signup}>
+                             <Form.Item
+                                 label="Fullname"
+                                 name="fullname"
+                                 rules={[{required: true}]}
+                             >
+                                 <Input size='large' placeholder='Ravi singh parihar' />
+                             </Form.Item>
+
+                             <Form.Item
+                                 label="Email"
+                                 name="email"
+                                 rules={[{required: true, type: 'email'}]}
+                             >
+                                 <Input size='large' placeholder='email@example.com' />
+                             </Form.Item>
+
+                             <Form.Item
+                                 label="Password"
+                                 name="password"
+                                 rules={[{required: true}]}
+                             >
+                                 <Input.Password size='large' placeholder='email@example.com' />
+                             </Form.Item>
+
+                             <Form.Item>
+                                 <Button htmlType='submit' size='large' type="primary" danger icon={<UserAddOutlined />}>Sign up</Button>
+                             </Form.Item>
+                         </Form>
+                         <div className='flex gap-2'>
+                             <p className='text-gray-500'>Already have an account ?</p>
+                             <Link href="/login">Sign in</Link>
+                         </div>
+                     </div>
+                 </Card>
+             </div>
+         </div>
+     )
+ }
+
+ export default Signup
+
+
+
